ecf091498b70d20de77266791c57de79b2a6c642
Josh <joshgiblett@iinet.net.au>
2013-04-16 20:48:16 +0800

    Fixed View Connections
    Fixed connections to Index, About, Contact, Login views on menu

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -31,10 +31,10 @@
 		array(
 			'class' => 'bootstrap.widgets.TbMenu',
 			'items' => array(
-				array('label' => 'Home', 'url' => array('/site/index')),
-				array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
-				array('label' => 'Contact', 'url' => array('/site/contact')),
-				array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+				array('label' => 'Home', 'url' => array('/Site/Index')),
+				array('label' => 'About', 'url' => array('/Site/Page', 'view' => 'about')),
+				array('label' => 'Contact', 'url' => array('/Site/Contact')),
+				array('label' => 'Login', 'url' => array('/Site/Login'), 'visible' => Yii::app()->user->isGuest),
 				array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
 			),
 		),
