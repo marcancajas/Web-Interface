bedefaffa0e2cce079148ef730aaea88ff79f6ad
Josh <joshgiblett@iinet.net.au>
2013-04-17 21:07:55 +0800

    Added backend main layout to fronted

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,56 +1,80 @@
-<?php
-/**
- * main.php
- *
- * @author: antonio ramirez <antonio@clevertech.biz>
- * Date: 7/23/12
- * Time: 12:31 AM
- */
-?>
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="language" content="en"/>
 
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/normalize.css">
-	
-	<!-- Use the .htaccess and remove these lines to avoid edge case issues. More info: h5bp.com/b/378 -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico" type="image/x-icon"/>
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css"
+	      media="screen, projection"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css"
+	      media="print"/>
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css"
+	      media="screen, projection"/>
+	<![endif]-->
 
-	<title><?php echo h($this->pageTitle); /* using shortcut for CHtml::encode */ ?></title>
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-
-
-	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css"/>
-	<!--using less instead? file not included-->
-	<!--<link rel="stylesheet/less" type="text/css" href="/less/styles.less">-->
-
-	<!-- create your own: http://modernizr.com/download/-->
-	<script src="<?php echo Yii::app()->request->baseUrl; ?>/app/js/libs/utils/modernizr-2.6.2.js"></script>
-
-	<!--<script src="/less/less-1.3.0.min.js"></script>-->
-	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico">
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
-<div class="container">
-	<?php echo $content?>
-</div>
 
-<!-- Google Analytics -->
-<script>
-	var _gaq=[['_setAccount','<?php echo param('google.analytics.account'); // check global.php shortcut file at "common/lib/" ?>'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-		s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
+<div class="container" id="page">
+	<?php $this->widget('bootstrap.widgets.TbNavbar', array(
+	'type' => 'inverse', // null or 'inverse'
+	'brand' => 'Project name',
+	'brandUrl' => '#',
+	'collapse' => true, // requires bootstrap-responsive.css
+	'items' => array(
+		array(
+			'class' => 'bootstrap.widgets.TbMenu',
+			'items' => array(
+				array('label' => 'Home', 'url' => array('/Site/Index')),
+				array('label' => 'About', 'url' => array('/Site/Page', 'view' => 'about')),
+				array('label' => 'Contact', 'url' => array('/Site/Contact')),
+				array('label' => 'Login', 'url' => array('/Site/Login'), 'visible' => Yii::app()->user->isGuest),
+				array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+			),
+		),
+		'<form class="navbar-search pull-left" action=""><input type="text" class="search-query span2" placeholder="Search"></form>',
+		(!Yii::app()->user->isGuest) ? '<p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>' : '',
+		array(
+			'class' => 'bootstrap.widgets.TbMenu',
+			'htmlOptions' => array('class' => 'pull-right'),
+			'items' => array(
+				array('label' => 'Link', 'url' => '#'),
+				'---',
+				array('label' => 'Dropdown', 'url' => '#', 'items' => array(
+					array('label' => 'Action', 'url' => '#'),
+					array('label' => 'Another action', 'url' => '#'),
+					array('label' => 'Something else here', 'url' => '#'),
+					'---',
+					array('label' => 'Separated link', 'url' => '#'),
+				)),
+			),
+		),
+	),
+)); ?>
+	<!-- mainmenu -->
+	<div class="container" style="margin-top:80px">
+		<?php if (isset($this->breadcrumbs)): ?>
+			<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+			'links' => $this->breadcrumbs,
+		)); ?><!-- breadcrumbs -->
+		<?php endif?>
+
+		<?php echo $content; ?>
+		<hr/>
+		<div id="footer">
+			Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+			All Rights Reserved.<br/>
+			<?php echo Yii::powered(); ?>
+		</div>
+		<!-- footer -->
+	</div>
+</div>
+<!-- page -->
 </body>
 </html>
