6229cdba86225210745bfdef0f71f944e10d1d59
Josh <joshgiblett@iinet.net.au>
2013-04-17 21:02:45 +0800

    Added UserController to Frontend

---
+++ frontend/controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+class SiteController extends Controller {
+
+	public function accessRules() {
+		return array(
+			// not logged in users
+			array('allow',
+				'users'=>array('*')),
+			// logged in users
+			array('allow',
+				'actions'=>array('index'),
+				'users' => array('@')),
+			// not logged in users can't do anything except above
+			array('deny',
+				'users'=>array('*')),
+		);
+	}
+
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+}
+?>
