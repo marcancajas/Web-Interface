4d8f7bb4594591fa9b5c667a844edc685409f807
Josh <joshgiblett@iinet.net.au>
2013-04-23 10:33:29 +0800

    Added new view for when user logs into website

---
+++ frontend/views/user/Index.php
@@ -0,0 +1,5 @@
+<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
+<h1>User View</h1>
+<?php echo CHtml::link('Connect to Worker',array('/User/ConnectToWorker')); ?>
+
+This is index view file...
