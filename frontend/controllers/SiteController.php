e7f4f4fa08dc208733edae56e4f01e51f354c4c8
Josh <joshgiblett@iinet.net.au>
2013-04-23 10:16:43 +0800

    Temporarily removed ajax validation for login controller

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -56,11 +56,13 @@ class SiteController extends Controller {
 	public function actionLogin() {
 		$model = new LoginForm;
 
-		// if it is ajax validation request
+		/*
+			// if it is ajax validation request
 		if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
+*/
 
 		// collect user input data
 		if (isset($_POST['LoginForm'])) {
