c572dffc1c8c8591363b7f01976885e9f385b6b7
Josh <joshgiblett@iinet.net.au>
2013-06-03 15:24:42 +0800

    Fixed user model rules for validation

--- common/models/User.php
+++ common/models/User.php
@@ -43,17 +43,28 @@ class User extends BaseUser
 	{
 		//Rules for database columns
 		return array(
-			array('email', 'email'),
+			//Check the password and username are not the same
+			array('newPassword', 'compare','operator' => '!=', 'compareAttribute' => 'username', 'message' => Yii::t('validation', "Password must not be the same as Username")),
+			//Check the email address is not already registered
+			array('email', 'unique', 'message' => Yii::t('user',"Email address already in use.")),
+			//Check the username is not already registered
+			array('username', 'unique', 'message' => Yii::t('user',"Username already in use.")),
+			//Check the passwords entered match
 			array('passwordConfirm', 'compare', 'compareAttribute' => 'newPassword', 'message' => Yii::t('validation', "Passwords don't match")),
+			//Set the required fields(firstname, lastname, password, cofirmPassword, country, birthdate, gender, username, email)
+			array('firstname, lastname, newPassword, passwordConfirm, username, email','required'),
+			//Set the minimum password length
 			array('newPassword', 'length', 'min' => 8),
-			array('username', 'length', 'min'=> 5),
-			//array('requires_new_password, login_attempts, login_time, create_id, create_time, update_id, update_time', 'numerical', 'integerOnly'=>true),
-			array('username', 'length', 'max'=>45),
-			array('password, salt, email, validation_key', 'length', 'max'=>255),
-			array('password_strategy, firstname, lastname', 'length', 'max'=>50),
-			//array('login_ip', 'length', 'max'=>32),
-			//array('username, password, salt, password_strategy, requires_new_password, email, login_attempts, login_time, login_ip, validation_key, create_id, create_time, update_id, update_time, firstname, lastname', 'default', 'setOnEmpty' => true, 'value' => null),
-			//array('id, username, password, salt, password_strategy, requires_new_password, email, login_attempts, login_time, login_ip, validation_key, create_id, create_time, update_id, update_time, firstname, lastname', 'safe', 'on'=>'search'),
+			//Check the password contains 2 numbers and 1 symbol($_*,!;)
+			array('newPassword', 'match', 'pattern' => '/^[a-zA-Z$_*,!;]*[_$*,!;]{1}[a-zA-Z$_*,!;]*[0-9]{1}[a-zA-Z0-9$_*,!;]*[0-9]{1}[a-zA-Z0-9$_*,!;]*|[a-zA-Z$_*,!;]*[0-9]{1}[a-zA-Z0-9$_*,!;]*[_$*,!;]{1}[a-zA-Z$_*,!;]*[0-9]{1}[a-zA-Z0-9$_*,!;]*|[a-zA-Z$_*,!;]*[0-9]{1}[a-zA-Z0-9$_*,!;]*[0-9]{1}[a-zA-Z0-9$_*,!;]*[_$*,!;]{1}[a-zA-Z$_*,!;]*$/u','message' => Yii::t('user',"Password must be minimum 8 digits long, contain 1 symbol(_$*,!;) and 2 numbers")),
+			//Set the minimum username length
+			array('username', 'length', 'min'=> 6),
+			//Check the username contains 1 number
+			array('username', 'match', 'pattern' => '/^[a-zA-Z0-9]*[0-9]{1}[a-zA-Z0-9]*$/u','message' => Yii::t('user',"Username must be minimum 6 digits long, contain letters and 1 number")),
+			//Checks the birthdate is in the correct format
+			array('birthdate', 'type', 'type' => 'date', 'message' => Yii::t('user',"{attribute} is not valid. Dates must be in the form dd-mm-yyyy"), 'dateFormat' => 'dd-mm-yyyy'),
+			//Set the searchable fields
+			//array('id, username, email, login_attempts, login_time, login_ip, create_id, create_time, update_id, update_time, firstname, lastname'				, 'gender', 'superuser', 'status', 'birthdate', 'safe', 'on'=>'search'),
 		);
 
 	}
