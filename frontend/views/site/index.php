fc90850221d2ab4c35a1e9d0de3e2ab405375831
Josh <joshgiblett@iinet.net.au>
2013-06-05 23:44:00 +0800

    Fixed image carousel in site/index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -33,10 +33,19 @@ $this->widget('bootstrap.widgets.TbButton',
 
                 <?php $this->widget('bootstrap.widgets.TbCarousel', array(
     'items'=>array(
-        array('image'=>'http://placehold.it/1150x300&text=First+thumbnail', 'label'=>'First Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-        array('image'=>'http://placehold.it/1150x300&text=Second+thumbnail', 'label'=>'Second Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-        array('image'=>'http://placehold.it/1150x300&text=Third+thumbnail', 'label'=>'Third Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-    ),
+        array('image'=>'http://placehold.it/1150x300&text=First+thumbnail',
+        		'label'=>'First Thumbnail label',
+        		//'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+				),
+        array('image'=>'http://placehold.it/1150x300&text=Second+thumbnail',
+        		'label'=>'Second Thumbnail label',
+       	 		//'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+	   	 		),
+        array('image'=>'http://placehold.it/1150x300&text=Third+thumbnail',
+        		'label'=>'Third Thumbnail label',
+        		//'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+				),
+		),
 )); ?>
             </div>
         </div>
