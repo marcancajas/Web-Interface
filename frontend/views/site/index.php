d3c91527c63edbceac483809e912e8171e775d90
Marc Ancajas <Marc Ancajas@Marc-PC>
2013-08-01 01:28:51 +0800

    renamed news_holder to left bar added right bar

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -19,47 +19,63 @@
 )); ?>
             </div>
         </div>
-
+ 
 <div id="bottom_container">
+       
+    
   
-        
-<div id="news_holder">
-    <div id="news">
+    <div id="left_bar">
+        <div id="main_content">
             <!-- @news content -->
             <h4>NEWS</h4>
-    	<div class="services-block">
+    	<div class="content">
                 <hr>
         	<img src="http://placehold.it/220x100&text=First+thumbnail" alt="Social marketing" />
           <h3><a href="#">Project Overview</a></h3>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. </p>
                
         </div>
-        <div class="services-block">
+        <div class="content">
                 <hr>
         	<img src="http://placehold.it/220x100&text=First+thumbnail" />
             <h3><a href="#">Second Semester Project Phase</a></h3>
         	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
              
         </div>
-        <div class="services-block">
+        <div class="content">
                 <hr>
         	<img src="http://placehold.it/220x100&text=First+thumbnail" />
             <h3><a href="#">Website Version 2.0</a></h3>
         	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
        
         </div>
-        <div class="services-block">
+        <div class="content">
                 <hr>
         	<img src="http://placehold.it/220x100&text=First+thumbnail" />
             <h3><a href="#">Any Questions?</a></h3>
         	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
             
         </div>
-        <div class="clear"></div>
-        More News...
-    </div>
-</div>
         
+        <a class="more_news" href="<?php echo Yii::app()->request->baseUrl; ?>/Site/News">More News...</a>
+           
+       </div>
+        
+         </div>
+    
+     <div id="right_bar">
+         
+    	<div class="content-block">
+              <h4>RANKINGS</h4>
+        </div>
+         
+       	<div class="content-block">
+              <h4>SERVER STATISTICS</h4>
+        </div>
     </div>
+    
+  
+      <div class="clear"></div>
+ </div>
 
 
