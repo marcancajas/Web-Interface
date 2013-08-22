d554d2adb2a0458e2de2c155038c941f34a9bfb9
Josh <joshgiblett@iinet.net.au>
2013-08-22 19:44:34 +0800

    Added JAMGame type model

---
+++ common/models/Type.php
@@ -0,0 +1,10 @@
+<?php
+
+Yii::import('common.models._base.BaseType');
+
+class Type extends BaseType
+{
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+}
