5a5b91f28096d83fef6ffc53a4d04417f519255b
Josh <joshgiblett@iinet.net.au>
2013-10-14 14:23:11 +0800

    Updated type views

--- backend/views/type/index.php
+++ backend/views/type/index.php
@@ -1,19 +1,48 @@
-<?php
+<!--Written by Josh Giblett-->
+<!--Character Type Index View-->
 
-$this->breadcrumbs = array(
-	Type::label(2),
-	Yii::t('app', 'Index'),
-);
+<!--Grid View Widget-->
+<?php $this->widget('bootstrap.widgets.TbGridView', array(
+		'type'=>'striped bordered',
+		'dataProvider'=>$dataProvider,
+		'template'=>"{items}",
+		'columns'=>array(
+			//name field
+			array(
+				'name'=>'name',
+				'header'=>'Name',
+				'headerHtmlOptions'=>array('style'=>'text-align:center; vertical-align:middle;'),
+				'htmlOptions'=>array('style'=>'text-align:center; vertical-align:middle;', 'width'=>'5px'),
+			),
+			//description field
+			array(
+				'name'=>'description',
+				'headerHtmlOptions'=>array('style'=>'text-align:center; vertical-align:middle;'),
+				'header'=>'Description',
+			),
+			//View, Update, Delete Buttons
+			array(
+				'class'=>'bootstrap.widgets.TbButtonColumn',
+				'template'=>'{view}&nbsp;{update}&nbsp;{delete}',
+				'buttons'=>array(
+					'view'=>array(
+						'options'=>array('style'=>'text-decoration:none;'),
+					),
+					'update'=>array(
+						'options'=>array('style'=>'text-decoration:none;'),
+					),
+					'delete'=>array(
+						'options'=>array('style'=>'text-decoration:none;'),
+					),
+				),
+				'viewButtonUrl'=>'app()->createUrl("Type/View",array("id"=>$data["id"]))',
+				'viewButtonIcon'=>'icon-tasks icon-large',
+				'updateButtonUrl'=>'app()->createUrl("Type/Update",array("id"=>$data["id"]))',
+				'updateButtonIcon'=>'icon-edit icon-large',
+				'deleteButtonUrl'=>'app()->createUrl("Type/Delete",array("id"=>$data["id"]))',
+				'deleteButtonIcon'=>'icon-remove icon-large',
+				'htmlOptions'=>array('style'=>'width: 40px; text-align:center'),
+			),
+		),
 
-$this->menu = array(
-	array('label'=>Yii::t('app', 'Create') . ' ' . Type::label(), 'url' => array('create')),
-	array('label'=>Yii::t('app', 'Manage') . ' ' . Type::label(2), 'url' => array('admin')),
-);
-?>
-
-<h1><?php echo GxHtml::encode(Type::label(2)); ?></h1>
-
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); 
+    )); ?>
